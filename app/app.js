215ff041323ab9f4bd58163f2a3123f72404811d
alicia-ciclos <91490602+alicia-ciclos@users.noreply.github.com>
2022-01-08 15:22:31 +0100

    Add CORS headers for accepting other domains

--- app/app.js
+++ app/app.js
@@ -11,6 +11,14 @@ const app = express();
 app.use(express.json())                                 // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
+// Accept CORS from any domain (just for running locally!)
+app.use((req, res, next) => {
+    res.append('Access-Control-Allow-Origin', ['*']);
+    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.append('Access-Control-Allow-Headers', 'Content-Type,Client-Token');
+    next();
+});
+
 app.get('/', async (req, res) => {
    res.send("OK");
 });
